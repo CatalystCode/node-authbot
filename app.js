c7eafce615df1ddd86dbfa2f24149bbe29fd62e7
Rita Zhang <rita.z.zhang@gmail.com>
2016-08-21 00:49:27 -0700

    🙈  Resume bot with secret

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ server.use(passport.initialize());
 
 server.get('/login', function(req, res, next) {
   console.log('get login');
+  console.log("passed in state: " + req.query.state);
   passport.authenticate('azuread-openidconnect', { failureRedirect: '/login', state: req.query.state }, function(err, user, info) {
     console.log('login callback');
     if (err) { return next(err); }
@@ -77,7 +78,9 @@ server.get('/api/OAuthCallback/',
   passport.authenticate('azuread-openidconnect', { failureRedirect: '/login', state: 'ritatest' }),
   function (req, res) {
     console.log('Returned from AzureAD.');
+    console.log('token: ' + req.query.token);
     console.log('state: ' + req.query.state);
+
     var botdata = req.query.state;
     console.log('botdata: ' + botdata);
     var addressId = botdata.split('|')[0];
@@ -103,15 +106,21 @@ server.get('/api/OAuthCallback/',
 
   var address = { id: addressId,
      channelId: 'webchat',
-     user: { id: 'FVCBFTKEVD2', name: 'FVCBFTKEVD2' },
+     user: { id: '6MOefHwNrNo', name: '6MOefHwNrNo' },
      conversation: { id: conversationId },
      bot: { id: 'authbot', name: 'authbot' },
      serviceUrl: 'https://webchat.botframework.com',
      useAuth: true };
 
-    console.log('before bot receive');
-    bot.receive(msg); 
-    console.log('after bot receive');
+  var user = { id: '6MOefHwNrNo', name: '6MOefHwNrNo' };
+
+    // var address = JSON.parse(req.query.state);
+    // console.log('address: ' + address);
+    var token = 'rita';
+    var code = 'zhang';
+
+    var continueMsg = new builder.Message().address(address).text("signin?token=" + token + "&code=" + code);
+    bot.receive(continueMsg);
     res.send('Welcome ' + req.user.displayName);
   });
 
@@ -162,12 +171,15 @@ if ( process.env.AUTHBOT_STRATEGY == 'oidStrategyv2') {
   strategy = oidStrategyv2;
   console.log('using v2');
 }
+
 passport.use(new OIDCStrategy(strategy,
   function(req, iss, sub, profile, accessToken, refreshToken, done) {
+    console.log('strategy returned');
   	//console.log(profile);
     //console.log('passport.use: ' + req);
 
     if (!profile.email) {
+      console.log('no profile email');
       return done(new Error("No email found"), null);
     }
     // asynchronous verification, for effect...
@@ -192,22 +204,81 @@ passport.use(new OIDCStrategy(strategy,
 //=========================================================
 // Bots Dialogs
 //=========================================================
-
-bot.dialog('/', function (session) {
-    console.log('bot dialog');
+function login(session) {
+    // Generate signin link
     console.log(session.message);
-    if (!session.userData.users) {
-        session.userData.users = [];
+
+    var resumptionCookie = JSON.stringify(session.message.address); // TODO: encrypt
+    console.log('creating resumptionCookie: ' + resumptionCookie);
+
+    var link = process.env.AUTHBOT_CALLBACKHOST + '/login?state=' + encodeURIComponent(resumptionCookie);
+    console.log(link);
+    builder.Prompts.text(session, "Please signin: " + link);
+}
+
+bot.dialog('/', [
+  function (session) {
+      session.beginDialog('signinPrompt');
+  },
+  function (session, results) {
+      if (results.response) {
+          // They're logged in
+          //var accessToken = session.privateConversationData.accessToken;
+
+      } else {
+          session.endConversation("Goodbye");
+      }
+  }
+]);
+
+bot.dialog('signinPrompt', [
+  function (session, args) {
+    console.log('signinPrompt');
+    if (args && args.invalid) {
+      // Re-prompt the user to click the link
+      builder.Prompts.text(session, "please click the signin link.");
     } else {
-
+        if (session.userData.refreshToken) {
+          //session.sendTyping();
+          // TODO: get access token from refresh token
+        } else {
+            login(session);
+        }
     }
-    console.log("message: " + session.message);
-    console.log("address: " + session.message.address);
-    console.log("convo: " + session.message.address.conversation.id);
-    //var state = querystring.stringify(session.message.address);
-    var state = session.message.address.id + "|" + session.message.address.conversation.id;
+  },
+  function (session, results) {
+    // Resume with "signin?token=<token>&code=<code>"
+    console.log('resumed result: ' + result.response);
 
-    console.log("state: " + state);
-    session.send("Hi there! Welcome! Please click %s to sign in: ", "https://authbot.azurewebsites.net/login?state=" + state);
-    
-});
+    var parts = results.response.split('?');
+    if (parts.length == 2 && parts[0] == 'signin') {
+      var params = parts[1].split('&');
+      if (params.length == 2 && params[0] == 'token') {
+          // Store token
+          session.dialogData.token = params[0];
+          session.dialogData.code = params[1];
+          builder.Prompts.text(session, "Please enter the code on the screen");
+      } else {
+          session.replaceDialog('signinPrompt', { invalid: true });
+      }
+    } else {
+        session.replaceDialog('signinPrompt', { invalid: true });
+    }
+  },
+  function (session, results) {
+      console.log('code result: ' + result.response);
+        var code = results.response;
+        if (code == session.dialogData.code) {
+            // // Store refresh token
+            // session.userData.refreshToken = session.dialogData.token;
+            // session.sendTyping();
+            // // ... Async call to convert refresh token to access token
+            // var accessToken = '';
+            // session.privateConversationData.accessToken = accessToken;
+            session.endDialog({ response: true });
+        } else {
+            session.send("I'm sorry... That was an invalid code");
+            session.endDialog({ response: false });
+        }
+  }
+]);
